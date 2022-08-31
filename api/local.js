a53f2e0c31049d4509ff3c2cdcd7df84b20a8bda
Rio Astamal <me@rioastamal.net>
2022-08-31 09:24:04 +0000

    API: Extract GET / to different Lambda function

--- api/local.js
+++ api/local.js
@@ -1,4 +1,5 @@
-const app = require('./src/index');
+const appExpressPath = process.env.APP_EXPRESS_PATH || './src/index';
+const app = require(appExpressPath);
 
 const port = process.env.APP_PORT || 8080;
 
